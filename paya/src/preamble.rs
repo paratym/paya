83fc27a08eb76ab1ecd5df663a8559242e9e13b3
pengiie <nathan@pengie.dev>
2024-04-24 14:13:36 -0700

    Added new buffer qualifiers

--- paya/src/preamble.rs
+++ paya/src/preamble.rs
@@ -8,7 +8,7 @@ pub const SHADER_PREAMBLE_GLSL: &str = "\
 #extension GL_EXT_buffer_reference : enable
 #extension GL_EXT_debug_printf : enable
 
-layout (set = 0, binding = 0) buffer BufferAddresses {
+layout (set = 0, binding = 0) readonly buffer BufferAddresses {
   uint64_t addresses[];
 } u_addresses;
 layout (set = 0, binding = 1, rgba8) uniform image2D u_images[100];
@@ -18,7 +18,9 @@ struct ResourceId {
 };
 
 #define DECL_PUSH_CONSTANTS layout(push_constant) uniform PushConstants
-#define DECL_BUFFER(alignment) layout(std430, buffer_reference, buffer_reference_align = alignment) buffer
+#define DECL_BUFFER(alignment) layout(std430, buffer_reference, buffer_reference_align = alignment) readonly buffer
+#define DECL_BUFFER_WRITE(alignment) layout(std430, buffer_reference, buffer_reference_align = alignment) writeonly buffer
+#define DECL_BUFFER_VOLATILE(alignment) layout(std430, buffer_reference, buffer_reference_align = alignment) volatile buffer
 #define DECL_BUFFER_COHERENT(alignment) layout(std430, buffer_reference, buffer_reference_align = alignment) coherent buffer
 
 #define get_buffer(id, type) type(u_addresses.addresses[id.index]);
